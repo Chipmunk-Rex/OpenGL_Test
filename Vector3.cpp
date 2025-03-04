70ae0f6d8e17162a7b707fbb5a8fd0ec19f62f32
Chipmunk-Rex <rexgood1005@gmail.com>
2025-03-04 09:24:40 -0400

    feat : Vector3

---
+++ Vector3.cpp
@@ -0,0 +1,22 @@
+struct Vector3
+{
+public:
+	int x;
+	int y;
+	int z;
+
+	Vector3(int x, int y, int z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	
+	Vector3 operator+(Vector3 other)
+	{
+		return Vector3(this->x + other.x, this->y + other.y, this->z + other.z);
+	}
+	Vector3 operator-(Vector3 other) {
+		return Vector3(this->x - other.x, this->y - other.y, this->z - other.z);
+	}
+};
